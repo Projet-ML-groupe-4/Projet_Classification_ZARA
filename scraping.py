9bceccc7ca9d22a321f836a8fc898ae6d1bb0e61
francis-mujani <francisqo2002@yahoo.fr>
2020-06-09 12:13:10 +0200

    modification de la multiprocessing

--- scraping.py
+++ scraping.py
@@ -30,6 +30,23 @@ from threading import Thread
 from time import sleep
 import multiprocessing
 
+# Les urls des chaques categories
+
+urls_ = ['https://www.zara.com/fr/fr/femme-robes-l1066.html?v1=1445722',
+         'https://www.zara.com/fr/fr/femme-jupes-l1299.html?v1=1445719',
+        'https://www.zara.com/fr/fr/femme-t-shirts-l1362.html?v1=1445717',
+        'https://www.zara.com/fr/fr/femme-pantalons-l1335.html?v1=1445724']
+
+
+# les paths pour enregistre les images
+
+file_zalando = (r"\Users\utilisateur\Desktop\ml_projet\Projet_Classification_ZARA\zara\jupes")
+file_robe = (r"\Users\utilisateur\Desktop\ml_projet\Projet_Classification_ZARA\zara\robes")
+file_jupe = (r"\Users\utilisateur\Desktop\ml_projet\Projet_Classification_ZARA\zara\jupes")
+file_tshi = (r"\Users\utilisateur\Desktop\ml_projet\Projet_Classification_ZARA\zara\tshirts")
+file_pant = (r"\Users\utilisateur\Desktop\ml_projet\Projet_Classification_ZARA\zara\pantalons")
+
+
 # telechargement des images
 
 def get_image(folder_path:str,url:str):
@@ -48,22 +65,6 @@ def get_image(folder_path:str,url:str):
         print(f"SUCCESS - saved {url} - as {file_path}")
     except Exception as e:
         print(f"ERROR - Could not save {url} - {e}")
-
-# Les urls des chaques categories
-
-urls_ = ['https://www.zara.com/fr/fr/femme-robes-l1066.html?v1=1445722',
-         'https://www.zara.com/fr/fr/femme-jupes-l1299.html?v1=1445719',
-        'https://www.zara.com/fr/fr/femme-t-shirts-l1362.html?v1=1445717',
-        'https://www.zara.com/fr/fr/femme-pantalons-l1335.html?v1=1445724']
-
-
-# les paths pour enregistre les images
-
-file_zalando = (r"\Users\utilisateur\Desktop\projet_ML\Projet_Classification_ZARA\zara\news")
-file_robe = (r"\Users\utilisateur\Desktop\projet_ML\Projet_Classification_ZARA\zara\robes")
-file_jupe = (r"\Users\utilisateur\Desktop\projet_ML\Projet_Classification_ZARA\zara\jupes")
-file_tshi = (r"\Users\utilisateur\Desktop\projet_ML\Projet_Classification_ZARA\zara\tshirts")
-file_pant = (r"\Users\utilisateur\Desktop\projet_ML\Projet_Classification_ZARA\zara\pantalons")
 
 
 # configurer webdriver
@@ -87,10 +88,11 @@ driver_4.get(urls_[3])
 # Fonction pour scrapper les Pantalon
 
 def pantalons_s():
-    for i in range(459):
+    time.sleep(4)
+    for i in range(3):
         try:
             liste_products_s = driver_4.find_elements_by_class_name('product')
-            print(f'nombres des robes {int(len(liste_products_s))-1}')
+            print(f'nombres des pantalons restants{int(len(liste_products_s))-1}')
 
             # click sur image
             liste_products_s[i].click()
@@ -109,7 +111,8 @@ def pantalons_s():
 # Fonction pour scrapper les Robes
 
 def robes_s():
-    for i in range(85, 652):
+    time.sleep(4)
+    for i in range(3):
         try:
             liste_products_s = driver_1.find_elements_by_class_name('product')
             print(f'nombres des robes restantes {int(len(liste_products_s))-i}')
@@ -118,7 +121,7 @@ def robes_s():
             time.sleep(4)
             # get images
             src_img = driver_1.find_element_by_css_selector("#main-images > div:nth-child(1) > a > img").get_attribute('src')
-            url_images.append(src_img)
+            #url_images.append(src_img)
             get_image(file_robe,src_img )
             print(f'robe numero : {i} enregistrée')
             driver_1.back()
@@ -139,7 +142,7 @@ def jupes_s():
             time.sleep(3)
             # get images
             src_img = driver_2.find_element_by_css_selector("#main-images > div:nth-child(1) > a > img").get_attribute('src')
-            url_images.append(src_img)
+            #url_images.append(src_img)
             print(f'src {i} enregistre')
             sleep(5)
             driver_2.back()
@@ -162,7 +165,7 @@ def tshirts_s():
             time.sleep(4)
             # get images
             src_img = driver_3.find_element_by_css_selector("#main-images > div:nth-child(1) > a > img.image-big._img-zoom._main-image").get_attribute('src')
-            url_images.append(src_img)
+            #url_images.append(src_img)
             get_image(file_tshi,src_img )
             print(f'tshirt numero {i} enregistré')
             sleep(2)
@@ -174,19 +177,30 @@ def tshirts_s():
 
 # multiprocessing
 
-start = time.perf_counter()
-pantalons_s()
-robes_s()
+if __name__ == '__main__':
+    start = time.perf_counter()
+    pantalons_s()
+    robes_s()
+    jupes_s
+    tshirts_s
+    p1 = multiprocessing.Process(target=pantalons_s)
+    p2 = multiprocessing.Process(target=robes_s)
+    p3 = multiprocessing.process(target=jupes_s)
+    p4 = multiprocessing.process(target=tshirts_s)
 
-p1 = multiprocessing.process(target=pantalons_s)
-p2 = multiprocessing.process(target=robes_s)
-p3 = multiprocessing.process(target=jupes_s)
-p4 = multiprocessing.process(target=tshirts_s)
+    p1.start()
+    p2.start()
+ 
+    p3.join()
+    p4.join()
+    finish = time.perf_counter()
+    print(f'Finished in {round(finish - start, 2)} secondes')
 
-p1.join()
-p2.join()
-p3.join()
-p4.join()
-
-finish = time.perf_counter()
-print(f'Finished in {round(finish - start, 2)} secondes')
+    #fonction =[pantalons_s, robes_s]
+    #args = [[4]]
+    #with multiprocessing.Pool(processes=4) as pool:
+    #    for i in range(0):
+    #        p[i] = pool.starmap(fonction[i], args)
+    #        p[i].start()
+    #        p[i].join()
+            
